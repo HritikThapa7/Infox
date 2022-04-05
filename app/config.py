06cf8001bd850775a073fe839fcd739a96fb8ab5
Hritik thapa <57740801+HritikThapa7@users.noreply.github.com>
2022-04-05 06:11:40 +0545

    Update mongo_uri

--- app/config.py
+++ app/config.py
@@ -1,3 +1,3 @@
 class Config:
     # UPLOAD_FOLDER = '/home/dmonk/Downloads/III_II Sem/SemProjectII/INFOX-API/app/UPLOADS'
-    MONGO_URI = 'mongodb+srv://HritikThapa7:infox123@cluster0.gechm.mongodb.net/infoxdb?retryWrites=true&w=majority'
+    MONGO_URI = 'mongodb+srv://<db_username>:<password_to_username>@cluster0.gechm.mongodb.net/db_name?retryWrites=true&w=majority'
